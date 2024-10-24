10bc7aaed471b9277d6396d8e3f178137d7d6fba
kamalpatelreactjsdeveloper <kamalpatel2262001@gmail.com>
2024-10-24 12:43:06 +0530

    main

--- src/componants/Rmc1.jsx
+++ src/componants/Rmc1.jsx
@@ -11,76 +11,82 @@ import rmc8 from "../image/rmc8.png";
 const Rmc1 = () => {
     return (
         <div className=" ">
-            <section className="bg-customtext text-white py-8 px-4 md:px-16">
-                <h2 className="2xl:max-w-[1440px] mx-auto text-center xl:leading-[80px] lg:leading-[40px]  font-bold mb-8 grid">
-                    <span className="xl:text-[50px] lg:text-[30px] text-[#e7e7e7] italic font-normal">  RANGE OF </span>
-                    <span className="text-white xl:text-[80px] lg:text-[50px] italic">SOLUTIONS</span>
+            <section className="bg-customtext text-white lg:py-8 md:py-4 lg:px-4 md:px-8 ">
+                <h2 className="2xl:max-w-[1440px] mx-auto text-center xl:leading-[80px] lg:leading-[40px] md:leading-[30px]  font-bold mb-8 grid">
+                    <span className="xl:text-[50px] lg:text-[30px] md:text-[22px] text-[#e7e7e7] italic font-normal">  RANGE OF </span>
+                    <span className="text-white xl:text-[80px] lg:text-[50px] md:text-[35px] italic">SOLUTIONS</span>
                 </h2>
 
                 <div className="2xl:max-w-[1440px] mx-auto w-full md:grid-cols-3 gap-8">
                     {/* Ordinary Section */}
-                    <div className="flex  py-6">
-                        <div className="gap-[50px] flex items-center">
-                            <h3 className="xl:text-[80px] lg:text-[50px] xl:px-10 font-bold text-[#01b3fd] mb-4">Ordinary</h3>
-                            <div className=" h-full  w-1 bg-white"></div>
+                    <div className="md:flex md:py-6">
+                        <div className="lg:gap-[50px] md:gap-[80px] md:flex items-center">
+                            <h3 className="xl:text-[80px] lg:text-[50px] md:text-[40px] xs:text-[40px] xs:text-center xl:px-10 font-bold text-[#01b3fd] mb-4">Ordinary</h3>
+                            <div className="md:h-full  md:w-1 xs:px-10 pb-3">
+                                <div className=" md:h-full  md:w-1 xs:w-full  xs:px-2 xs:h-1  bg-white"></div>
+                            </div>
                         </div>
-                        <ul className="pl-8 space-y-4 xl:text-[30px] lg:text-[20px]">
+                        <ul className="pl-8 space-y-4 xl:text-[30px] lg:text-[20px] ">
                             <li className="xl:leading-[34px] lg:leading-[20px]">
                                 <p className="font-medium">M10 PCC (Plain Cement Concrete)</p>
-                                <p className="font-thin xl:text-[25px] lg:text-[20px] text-[#f4f4f5]">e.g. Levelling course,bedding for footing,concrete roads,etc.</p>
+                                <p className="font-thin xl:text-[25px] lg:text-[20px] md:text-[15px] text-[#f4f4f5]">e.g. Levelling course,bedding for footing,concrete roads,etc.</p>
                             </li>
                             <li className="xl:leading-[34px] lg:leading-[20px]">
                                 <p className="font-medium">M15 PCC</p>
-                                <p className="font-thin xl:text-[25px] lg:text-[20px] text-[#f4f4f5]">e.g. Levelling course,bedding for footing,concrete roads,etc.</p>
+                                <p className="font-thin xl:text-[25px] lg:text-[20px] md:text-[15px] text-[#f4f4f5]">e.g. Levelling course,bedding for footing,concrete roads,etc.</p>
                             </li>
                             <li className="xl:leading-[34px] lg:leading-[20px]">
                                 <p className="font-medium">M20 RCC (Reinforced Cement Concrete)</p>
-                                <p className="font-thin xl:text-[25px] lg:text-[20px] text-[#f4f4f5]">e.g.Slabs,beams,columns,footings,etc.(for mild exposure)</p>
+                                <p className="font-thin xl:text-[25px] lg:text-[20px] md:text-[15px] text-[#f4f4f5]">e.g.Slabs,beams,columns,footings,etc.(for mild exposure)</p>
                             </li>
                         </ul>
                     </div>
 
-                    <div className="flex  py-6">
-                        <div className="xl:gap-[37px] lg:gap-[40px] flex items-center ">
-                            <h3 className="xl:text-[80px] lg:text-[50px] xl:px-10 font-bold text-[#01b3fd] mb-4">Standard</h3>
-                            <div className="h-full  w-1 bg-white"></div>
+                    <div className="md:flex  md:py-6">
+                        <div className="xl:gap-[37px] lg:gap-[40px] md:gap-[73px] md:flex items-center ">
+                            <h3 className="xl:text-[80px] lg:text-[50px] md:text-[40px] xs:text-[40px] xs:pt-5 xs:text-center xl:px-10 font-bold text-[#01b3fd] mb-4">Standard</h3>
+                            <div className="md:h-full  md:w-1 xs:px-10 pb-3">
+                                <div className=" md:h-full  md:w-1 xs:w-full  xs:px-2 xs:h-1  bg-white"></div>
+                            </div>
                         </div>
                         <ul className="pl-8 space-y-4 xl:text-[30px] lg:text-[20px] ">
                             <li className="xl:leading-[34px] lg:leading-[20px]">
                                 <p className="font-medium">M25 RCC (Reinforced Cement Concrete)</p>
-                                <p className="font-thin xl:text-[25px] lg:text-[20px] text-[#f4f4f5]">e.g. Slabs,beams,columns,footings,etc.</p>
+                                <p className="font-thin xl:text-[25px] lg:text-[20px] md:text-[15px] text-[#f4f4f5]">e.g. Slabs,beams,columns,footings,etc.</p>
                             </li>
                             <li className="xl:leading-[34px] lg:leading-[20px]">
                                 <p className="font-medium">M30 RCC</p>
-                                <p className="font-thin xl:text-[25px] lg:text-[20px] text-[#f4f4f5]">e.g. Slabs,beams,columns,footings,etc.</p>
+                                <p className="font-thin xl:text-[25px] lg:text-[20px] md:text-[15px] text-[#f4f4f5]">e.g. Slabs,beams,columns,footings,etc.</p>
                             </li>
                             <li className="xl:leading-[34px] lg:leading-[20px]">
                                 <p className="font-medium">M35 RCC</p>
-                                <p className="font-thin xl:text-[25px] lg:text-[20px] text-[#f4f4f5]">e.g. Slabs,beams,columns,footings,etc.</p>
+                                <p className="font-thin xl:text-[25px] lg:text-[20px] md:text-[15px] text-[#f4f4f5]">e.g. Slabs,beams,columns,footings,etc.</p>
                             </li>
                             <li className="xl:leading-[34px] lg:leading-[20px]">
                                 <p className="font-medium">M40 RCC</p>
-                                <p className="font-thin xl:text-[25px] lg:text-[20px] text-[#f4f4f5]">e.g. Pre-stressed concrete,slabs,beams,columns,footings,etc.</p>
+                                <p className="font-thin xl:text-[25px] lg:text-[20px] md:text-[15px] text-[#f4f4f5]">e.g. Pre-stressed concrete,slabs,beams,columns,footings,etc.</p>
                             </li>
                             <li className="xl:leading-[34px] lg:leading-[20px]">
                                 <p className="font-medium">M45 RCC</p>
-                                <p className="font-thin xl:text-[25px] lg:text-[20px] text-[#f4f4f5]">e.g. Runways,Concrete Roads(PQC),Prestressed Concrete Girders,RCC Columns,Prestressed beams</p>
+                                <p className="font-thin xl:text-[25px] lg:text-[20px] md:text-[15px] text-[#f4f4f5]">e.g. Runways,Concrete Roads(PQC),Prestressed Concrete Girders,RCC Columns,Prestressed beams</p>
                             </li>
                             <li className="xl:leading-[34px] lg:leading-[20px]">
                                 <p className="font-medium">M50 RCC</p>
-                                <p className="font-thin xl:text-[25px] lg:text-[20px] text-[#f4f4f5]">e.g. Runways,Concrete Roads (PQC),Prestressed Concrete Girders,RCC Columns,Prestressed beams</p>
+                                <p className="font-thin xl:text-[25px] lg:text-[20px] md:text-[15px] text-[#f4f4f5]">e.g. Runways,Concrete Roads (PQC),Prestressed Concrete Girders,RCC Columns,Prestressed beams</p>
                             </li>
                             <li className="xl:leading-[34px] lg:leading-[20px]">
                                 <p className="font-medium">M55 RCC</p>
-                                <p className="font-thin xl:text-[25px] lg:text-[20px] text-[#f4f4f5]">e.g. Prestressed Concrete Girders and Piers</p>
+                                <p className="font-thin xl:text-[25px] lg:text-[20px] md:text-[15px] text-[#f4f4f5]">e.g. Prestressed Concrete Girders and Piers</p>
                             </li>
                         </ul>
                     </div>
 
-                    <div className="flex  py-6">
-                        <div className=" xl:gap-[53px] lg:gap-[50px] flex items-center">
-                            <h3 className="xl:text-[80px] lg:text-[50px] xl:leading-[80px] text-center lg:leading-[50px] xl:px-10 font-bold text-[#01b3fd] mb-4">High Strength</h3>
-                            <div className="h-full  w-[6px] bg-white"></div>
+                    <div className="md:flex  md:py-6">
+                        <div className=" xl:gap-[53px] lg:gap-[50px] md:gap-[80px] md:flex items-center ">
+                            <h3 className="xl:text-[80px] lg:text-[50px] md:text-[40px] xs:text-[40px] xs:pt-5 xl:leading-[80px] text-center lg:leading-[50px] xl:px-10 font-bold text-[#01b3fd] mb-4">High Strength</h3>
+                            <div className="md:h-full  md:w-1 xs:px-10 pb-3">
+                                <div className=" md:h-full  md:w-[6px] xs:w-full  xs:px-2 xs:h-1  bg-white"></div>
+                            </div>
                         </div>
                         <ul className="pl-8 space-y-4 xl:text-[30px] lg:text-[20px]">
                             <li>
@@ -91,8 +97,8 @@ const Rmc1 = () => {
                             </li>
                             <li className="xl:leading-[34px] lg:leading-[20px]">
                                 <p className="font-medium">M80</p>
-                                <p className="font-thin xl:text-[25px] lg:text-[20px] text-[#f4f4f5]">RCC work Where high compressive strength is required such as high rise buildings,long span bridges, ultra-thin white topping etc and constructions in aggressive environment
-                                    <br/>e.g. Spillways of dams, coastal construction</p>
+                                <p className="font-thin xl:text-[25px] lg:text-[20px] md:text-[15px] text-[#f4f4f5]">RCC work Where high compressive strength is required such as high rise buildings,long span bridges, ultra-thin white topping etc and constructions in aggressive environment
+                                    <br />e.g. Spillways of dams, coastal construction</p>
                             </li>
                         </ul>
                     </div>
@@ -100,46 +106,46 @@ const Rmc1 = () => {
             </section>
 
             <section className="bg-white py-8 2xl:max-w-[1440px] mx-auto ">
-                <h2 className="text-center xl:xl:text-[80px] lg:text-[50px] font-bold italic  text-customtext mb-8">
+                <h2 className="text-center xl:xl:text-[80px] lg:text-[50px] md:text-[40px] font-bold italic  text-customtext mb-8">
                     ADVANTAGES
                 </h2>
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 px-4 md:px-16 text-blue-900">
+                <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-3 gap-8 px-4 md:px-8 text-blue-900">
                     <div className="gap-8 px-4 ">
                         <div className="flex py-2 items-center space-x-4">
-                            <img src={rmc1} className="xl:w-24 xl:h-24 lg:w-16 lg:h-16" />
-                            <p className="xl:text-[25px] lg:text-[15px] xl:leading-[25px] lg:leading-[20px] font-normal text-black">Reduced embodied <br/> carbon emissions</p>
+                            <img src={rmc1} className="xl:w-24 xl:h-24 lg:w-16 lg:h-16 md:w-12 md:h-12" />
+                            <p className="xl:text-[25px] lg:text-[15px] md:text-[11px] xl:leading-[25px] lg:leading-[20px] font-normal text-black">Reduced embodied <br /> carbon emissions</p>
                         </div>
                         <div className="flex py-2 items-center space-x-4">
-                            <img src={rmc2} alt="Icon 2" className="xl:w-24 xl:h-24 lg:w-16 lg:h-16" />
-                            <p className="xl:text-[25px] lg:text-[15px] xl:leading-[25px] lg:leading-[20px] font-normal text-black">Speedier construction</p>
+                            <img src={rmc2} alt="Icon 2" className="xl:w-24 xl:h-24 lg:w-16 lg:h-16 md:w-12 md:h-12" />
+                            <p className="xl:text-[25px] lg:text-[15px] md:text-[11px] xl:leading-[25px] lg:leading-[20px] font-normal text-black">Speedier construction</p>
                         </div>
                         <div className="flex py-2 items-center space-x-4">
-                            <img src={rmc3} alt="Icon 3" className="xl:w-24 xl:h-24 lg:w-16 lg:h-16" />
-                            <p className="xl:text-[25px] lg:text-[15px] xl:leading-[25px] lg:leading-[20px] font-normal text-black">Better handling and <br/> proper mixing practices</p>
-                        </div>
-                    </div>
-                    <div className="gap-8 px-4">
-                        <div className="flex py-2 items-center space-x-4">
-                            <img src={rmc4} alt="Icon 4" className="xl:w-24 xl:h-24 lg:w-16 lg:h-16" />
-                            <p className="xl:text-[25px] lg:text-[15px] xl:leading-[25px] lg:leading-[20px] font-normal text-black">Enables sustainable construction</p>
-                        </div>
-                        <div className="flex py-2 items-center space-x-4">
-                            <img src={rmc5} alt="Icon 5" className="xl:w-24 xl:h-24 lg:w-16 lg:h-16" />
-                            <p className="xl:text-[25px] lg:text-[15px] xl:leading-[25px] lg:leading-[20px] font-normal text-black">Savings on lifecycle cost</p>
-                        </div>
-                        <div className="flex py-2 items-center space-x-4">
-                            <img src={rmc6} alt="Icon 6" className="xl:w-24 xl:h-24 lg:w-16 lg:h-16" />
-                            <p className="xl:text-[25px] lg:text-[15px] xl:leading-[25px] lg:leading-[20px] font-normal text-black">Reduced dependency on intensive labour thus, lesser errors</p>
+                            <img src={rmc3} alt="Icon 3" className="xl:w-24 xl:h-24 lg:w-16 lg:h-16 md:w-12 md:h-12" />
+                            <p className="xl:text-[25px] lg:text-[15px] md:text-[11px] xl:leading-[25px] lg:leading-[20px] font-normal text-black">Better handling and <br /> proper mixing practices</p>
                         </div>
                     </div>
                     <div className="gap-8 px-4">
                         <div className="flex py-2 items-center space-x-4">
-                            <img src={rmc7} alt="Icon 7" className="xl:w-24 xl:h-24 lg:w-16 lg:h-16" />
-                            <p className="xl:text-[25px] lg:text-[15px] xl:leading-[25px] lg:leading-[20px] font-normal text-black">Crafted concrete solutions for various applications</p>
+                            <img src={rmc4} alt="Icon 4" className="xl:w-24 xl:h-24 lg:w-16 lg:h-16 md:w-12 md:h-12" />
+                            <p className="xl:text-[25px] lg:text-[15px] md:text-[11px] xl:leading-[25px] lg:leading-[20px] font-normal text-black">Enables sustainable construction</p>
                         </div>
                         <div className="flex py-2 items-center space-x-4">
-                            <img src={rmc8} alt="Icon 8" className="xl:w-24 xl:h-24 lg:w-16 lg:h-16" />
-                            <p className="xl:text-[25px] lg:text-[15px] xl:leading-[25px] lg:leading-[20px] font-normal text-black">Well-trained technical team and advanced</p>
+                            <img src={rmc5} alt="Icon 5" className="xl:w-24 xl:h-24 lg:w-16 lg:h-16 md:w-12 md:h-12" />
+                            <p className="xl:text-[25px] lg:text-[15px] md:text-[11px] xl:leading-[25px] lg:leading-[20px] font-normal text-black">Savings on lifecycle cost</p>
+                        </div>
+                        <div className="flex py-2 items-center space-x-4">
+                            <img src={rmc6} alt="Icon 6" className="xl:w-24 xl:h-24 lg:w-16 lg:h-16 md:w-12 md:h-12" />
+                            <p className="xl:text-[25px] lg:text-[15px] md:text-[11px] xl:leading-[25px] lg:leading-[20px] font-normal text-black">Reduced dependency on intensive labour thus, lesser errors</p>
+                        </div>
+                    </div>
+                    <div className="gap-8 px-4">
+                        <div className="flex py-2 items-center space-x-4">
+                            <img src={rmc7} alt="Icon 7" className="xl:w-24 xl:h-24 lg:w-16 lg:h-16 md:w-12 md:h-12" />
+                            <p className="xl:text-[25px] lg:text-[15px] md:text-[11px] xl:leading-[25px] lg:leading-[20px] font-normal text-black">Crafted concrete solutions for various applications</p>
+                        </div>
+                        <div className="flex py-2 items-center space-x-4">
+                            <img src={rmc8} alt="Icon 8" className="xl:w-24 xl:h-24 lg:w-16 lg:h-16 md:w-12 md:h-12" />
+                            <p className="xl:text-[25px] lg:text-[15px] md:text-[11px] xl:leading-[25px] lg:leading-[20px] font-normal text-black">Well-trained technical team and advanced</p>
                         </div>
                     </div>
                 </div>
--- src/componants/Rmcbannner.jsx
+++ src/componants/Rmcbannner.jsx
@@ -11,19 +11,19 @@ const Rmcbanner = () => {
             />
 
             <div className="absolute inset-0 w-1/2  flex items-center justify-start px-8 lg:px-20 pt-0 ">
-                <div className="text-black  ">
-                    <p className=" xs:text-xl italic text-black md:text-xl lg:text-[52px]  mb-7 "> We provide range of   </p>
+                <div className="text-black mt-[-60px] ">
+                    <p className=" xs:text-md italic text-black md:text-3xl xl:text-[52px] lg:text-[38px] xl:mb-7 lg:mb-4 "> We provide range of   </p>
                     <div className="flex italic">
-                        <h1 className="text-3xl xs:text-xl text-customtext md:text-5xl lg:text-[64px] font-bold ">SOLUTIONS </h1>
-                        <p className="pt-5 xs:text-xl text-black md:text-xl lg:text-[52px] pl-2">in</p>
+                        <h1 className="text-3xl xs:text-xl text-customtext md:text-4xl lg:text-5xl xl:text-[64px]  font-bold ">SOLUTIONS </h1>
+                        <p className="xl:pt-5  lg:pt-3 md:pt-[3px] xs:text-md xs:pl-[2px] xs:pt-[3px] text-black md:text-3xl xl:text-[52px] lg:text-[38px] pl-2">in</p>
                     </div>
-                    <h1 className="text-3xl italic xs:text-xl text-customtext md:text-5xl lg:text-[64px] pt-2  font-bold ">
+                    <h1 className="text-3xl italic xs:text-xl text-customtext lg:text-5xl xl:text-[64px] lg:pt-2  font-bold ">
                         RMC
                     </h1>
                 </div>
             </div>
             <div>
-                <h3 className="text-start text-2xl bg-white text-black p-5 pl-16 lg:pl-[70px]">
+                <h3 className="text-start text-2xl xs:text-[15px] bg-white text-black p-5 pl-16 lg:pl-[70px]">
                     Home &gt; <span className="text-customtext font-semibold">RMC</span>
                 </h3>
             </div>
